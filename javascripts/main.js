ab414fa80fb479d121ad088389b0845090451250
Timothy Harley <timothy.d.harley@gmail.com>
2018-10-16 18:44:23 -0500

    loading fish from json

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,38 @@
 
 // Add fish to "Basket"
 
+const writeFishes = (arrayOfFishes) => {
+    let domString = '';
+    arrayOfFishes.forEach((fish)=> { //in the dollar sign brackets is asking if "onSale" is true//
+        domString += `
+        <div class="${fish.onSale ? 'on-sale' : ''} card col-md-6 col-md-offset-3">
+                <div class="thumbnail">
+                    <img src="${fish.imageSoure}"
+                        alt="" width="40%">
+                    <div class="caption">
+                        <h3 id="thumbnail-label">${fish.name}</h3>
+                        <p>$
+                            <span class="price">${fish.basePrice}</span>
+                        </p>
+                    </div>
+                    <div class="caption card-footer">
+                        <button class="add btn btn-danger">Add To Basket</button>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+    //write to the available div
+    $('#available').append(domString);
+}
 
 // Load Fish
 $.get('../db/fishes.json')
     .done((data)=> {
         console.log(data);
+        writeFishes(data.fishes);
     })
     .fail((error)=> {
         console.error(error);
     });
+
